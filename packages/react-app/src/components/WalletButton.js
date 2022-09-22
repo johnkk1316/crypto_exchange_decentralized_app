038a7e17965f2e66c1bd04f7e791f24909bc1c58
John KK <johnkk1316@protonmail.com>
2022-09-23 01:35:45 +0300

    Added exchange, loader and walletbutton components

---
+++ packages/react-app/src/components/WalletButton.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react'
+import {shortenAddress, useEthers, useLookupAddress } from '@usedapp/core';
+
+import styles from '../styles';
+
+const WalletButton = () => {
+    const [accountAddress, setAccountAddress] = useState('');
+
+    const { ens } = useLookupAddress();
+    const { account, deactivate, activateBrowserWallet } = useEthers();
+    
+    useEffect(() => {
+        if( ens ) {
+            setAccountAddress(ens);
+        } else if ( account ) {
+            setAccountAddress(shortenAddress(account));
+        } else {
+            setAccountAddress('');
+        }
+    }, [account, ens, setAccountAddress]);
+
+    return (
+        <button
+            onClick={() =>{
+                if (!account) {
+                    activateBrowserWallet();
+                } else {
+                    deactivate();
+                }
+            }}
+            className={styles.walletButton}
+        >
+            { accountAddress || "Connect Wallet" }
+        </button>
+    )
+}
+
+export default WalletButton
