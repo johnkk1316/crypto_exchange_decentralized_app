36871ed5a5a8cb4f8203a01f4cec58502951f182
John KK <johnkk1316@protonmail.com>
2022-09-22 07:37:03 +0300

    commenced appjs

--- packages/react-app/src/App.js
+++ packages/react-app/src/App.js
@@ -1,7 +1,25 @@
 import React from "react"
+import { useEthers } from "@usedapp/core";
+
+import styles from "./styles";
+import { uniswapLogo } from "./assets";
 
 const App = () => {
-  return <p>Hello, Web3!</p>
+  return (
+    <div className={styles.container}>
+        <div className={styles.innerContainer}>
+            <header className={styles.header}>
+              <img
+                  src={uniswapLogo}
+                  alt="uniswap-logo"
+                  className="w-16 h-16 object-contain"
+              />
+              WalletButton 
+            </header>
+            
+        </div>
+    </div>
+  )
 }
 
 export default App;
