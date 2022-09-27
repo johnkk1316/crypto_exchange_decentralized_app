8e6596154c98ea3b29dcb0358ffa50eb635eabb6
John KK <johnkk1316@protonmail.com>
2022-09-27 21:14:54 +0300

    Added pools components in app.js and exchange.js

--- packages/react-app/src/App.js
+++ packages/react-app/src/App.js
@@ -1,14 +1,14 @@
 import React from "react"
 import { useEthers } from "@usedapp/core";
 
+import { usePools } from "./hooks";
 import styles from "./styles";
 import { uniswapLogo } from "./assets";
 import { Exchange, Loader, WalletButton } from "./components";
 
 const App = () => {
   const { account } = useEthers();
-
-  const poolsLoading = false;
+  const [loading, pools] = usePools();
 
   return (
     <div className={styles.container}>
@@ -31,9 +31,9 @@ const App = () => {
                     <div className="pink_gradient" />
                     <div className={styles.exchange}>
                       {account ? (
-                        poolsLoading ? (
+                        loading ? (
                           <Loader title="Loading pools, please wait!" />
-                        ) : <Exchange />
+                        ) : <Exchange pools={ pools }/>
                       ) : <Loader title="Please connect your wallet" />} 
                     </div>
                     <div className="blue_gradient" />
--- packages/react-app/src/components/Exchange.js
+++ packages/react-app/src/components/Exchange.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Contract } from "@ethersproject/contracts";
+import { abis } from "@my-app/contracts";
+import { ERC20, useContractFunction, useEthers, useTokenAllowance, useTokenBalance } from "@usedapp/core";
+import { ethers } from "ethers";
+import { parseUnits } from "ethers/lib/utils";
 
-const Exchange = () => {
-    return (
+
+import { ROUTER_ADDRESS } from "../config";
+
+
+const Exchange = ({ pools }) => {
+    return(
         <div>Exchange</div>
     )
-}
+};
 
-export default Exchange
+export default Exchange;
