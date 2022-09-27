44d3e636dca184ccd08f4df68edb3baa423d2710
John KK <johnkk1316@protonmail.com>
2022-09-27 20:28:33 +0300

    Updated utils components

--- packages/react-app/src/utils/index.js
+++ packages/react-app/src/utils/index.js
@@ -1,6 +1,6 @@
-export { getFactoryInfo } from './getFactoryInfo';
-export { getPairsInfo } from './getPairsInfo'
-export { getRouterInfo } from './getRouterInfo';
+export { getFactoryInfo } from "./getFactoryInfo";
+export { getPairsInfo } from "./getPairsInfo";
+export { getRouterInfo } from "./getRouterInfo";
 
 export { 
   getAvailableTokens,
