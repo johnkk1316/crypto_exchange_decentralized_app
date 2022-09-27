b4ed218a7a1f416466a5552637c01504e1ff5d3b
John KK <johnkk1316@protonmail.com>
2022-09-27 17:26:40 +0300

    added router address and URL

--- packages/react-app/src/config.js
+++ packages/react-app/src/config.js
@@ -1,10 +1,11 @@
 import { Goerli } from "@usedapp/core";
 
-export const ROUTER_ADDRESS = "[YOUR ADDRESS HERE]"; 
+
+export const ROUTER_ADDRESS = "0x62CB4AF34f9879e09aE624b671E997Ca1e362296"; 
 
 export const DAPP_CONFIG = {
   readOnlyChainId: Goerli.chainId,
   readOnlyUrls: {
-    [Goerli.chainId]: "[YOUR URL HERE]",
+    [Goerli.chainId]: "https://eth-goerli.g.alchemy.com/v2/Uza72i77Rm0JI7mgTMTks135VApNXf3y",
   },
 };
