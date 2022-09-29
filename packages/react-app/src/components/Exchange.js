9a2b7e0990ec4689456bd88f07ffeb43ed0ec42d
John KK <johnkk1316@protonmail.com>
2022-09-29 13:14:18 +0300

    imported the helper functions

--- packages/react-app/src/components/Exchange.js
+++ packages/react-app/src/components/Exchange.js
@@ -5,13 +5,30 @@ import { ERC20, useContractFunction, useEthers, useTokenAllowance, useTokenBalan
 import { ethers } from "ethers";
 import { parseUnits } from "ethers/lib/utils";
 
-
+import { getAvailableTokens, getCounterpartTokens, findPoolByTokens, isOperationPending, getFailureMessage, getSuccessMessage } from '../utils';
 import { ROUTER_ADDRESS } from "../config";
+import { AmountIn, AmountOut, Balance } from "./";
+import styles from "../styles";
 
 
 const Exchange = ({ pools }) => {
     return(
-        <div>Exchange</div>
+        <div className="flex flex-col w-full items-center">
+
+            <div className="mb-8">
+                <AmountIn 
+                    
+                />
+                <Balance/>
+            </div>
+
+            <div className="mb-8 w-[100%]">
+                <AmountOut
+                    
+                />
+                <Balance/>
+            </div>
+        </div>
     )
 };
 
