043e362b165eda519406c63f430953c7712b61de
John KK <johnkk1316@protonmail.com>
2022-09-30 23:24:09 +0300

    amountout component complete

--- packages/react-app/src/components/AmountOut.js
+++ packages/react-app/src/components/AmountOut.js
@@ -1,52 +1,67 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect, useRef } from "react";
+import { formatUnits } from "ethers/lib/utils";
 
-import { chevronDown } from '../assets'
-import styles from '../styles'
-import { useOnClickOutside } from '../utils'
+import { chevronDown } from "../assets";
+import { useAmountsOut, useOnClickOutside } from "../utils";
+import styles from "../styles";
 
-const AmountIn = () => {
-    const [ showList, setShowList ] = useState(false);
+    const AmountOut = ({ fromToken, toToken, amountIn, pairContract, currencyValue, onSelect, currencies }) => {
+    const [showList, setShowList] = useState(false);
+    const [activeCurrency, setActiveCurrency] = useState("Select");
+    const ref = useRef()
 
+    const amountOut = useAmountsOut(pairContract, amountIn, fromToken, toToken) ?? 0;
+
+    useOnClickOutside(ref, () => setShowList(false))
+
+    useEffect(() => {
+        if (Object.keys(currencies).includes(currencyValue)) {
+        setActiveCurrency(currencies[currencyValue]);
+        } else {
+        setActiveCurrency("Select")
+        }
+    }, [currencyValue, currencies]);
 
     return (
         <div className={styles.amountContainer}>
-            <input
-                placeholder="0.0"
-                type="number"
-                value=""
-                disabled="false"
-                onChange={() => {}}
-                className={styles.amountInput}
+        <input
+            placeholder="0.0"
+            type="number"
+            value={formatUnits(amountOut)}
+            className={styles.amountInput}
+            disabled
+        />
+
+        <div className="relative" onClick={() => setShowList(!showList)}>
+            <button className={styles.currencyButton}>
+            {activeCurrency}
+            <img
+                src={chevronDown}
+                alt="cheveron-down"
+                className={`w-4 h-4 object-contain ml-2 ${showList ? "rotate-180" : "rotate-0"}`}
             />
+            </button>
 
-            <div className= "relative" onClick={() => setShowList ((prevState) => !prevState)}>
-                <button className={ styles.currencyButton }>
-                    {"ETH"}
-                    <img
-                        src={chevronDown}
-                        alt="chevron down"
-                        className={`w-4 h-4 object-contain ml-2 ${showList ? `rotate-180` : `rotate-0`}`}
-                    />
-                </button>
-
-                {showList && (
-                    <ul className={styles.currencyList}>
-                        {[
-                            { token: 'ETH', tokenName: 'ETH' },
-                            { token: 'CDG', tokenName: 'CDG' }
-                        ].map(({token, tokenName}, index) => (
-                            <li
-                                key={ index }
-                                className={` ${styles.currencyListItem} ${true ? `bg-site-dim2` : ``} cursor-pointer`}
-                            >
-                                {tokenName}
-                            </li>
-                        ))}
-                    </ul>
-                )}
-            </div>
+            {showList && (
+            <ul ref={ref} className={styles.currencyList}>
+                {Object.entries(currencies).map(([token, tokenName], index) => (
+                <li
+                    key={index}
+                    className={styles.currencyListItem}
+                    onClick={() => {
+                    if (typeof onSelect === "function") onSelect(token);
+                    setActiveCurrency(tokenName);
+                    setShowList(false);
+                    }}
+                >
+                    {tokenName}
+                </li>
+                ))}
+            </ul>
+            )}
         </div>
-    )
-}
+        </div>
+    );
+    };
 
-export default AmountIn
+    export default AmountOut;
