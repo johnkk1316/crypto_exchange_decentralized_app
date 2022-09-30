e82b483fd12fce302b2153c6f0b1126c5a23abce
John KK <johnkk1316@protonmail.com>
2022-09-30 15:46:36 +0300

    done with the balance component for now

--- packages/react-app/src/components/Balance.js
+++ packages/react-app/src/components/Balance.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import { formatUnits, parseUnits } from 'ethers/lib/utils';
+
+import styles from '../styles';
 
 const Balance = () => {
+    const tokenBalance = parseUnits("0.00251")
+
     return (
-        <div>Balance</div>
+        <div className={styles.balance}>
+            <p className={styles.balanceText}>
+                {tokenBalance && (
+                    <>
+                        <span className={styles.balanceBold}>Balance: </span>
+                        {formatUnits(tokenBalance || parseUnits("0") )}
+                    </>
+                )}
+            </p>
+        </div>
     )
 }
 
